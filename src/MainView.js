bb25df22a6773e03086b06a337481af18d0dbfcd
zhaoxiangyu <liseami@qq.com>
2023-04-12 12:23:40 +0800

    zsh快捷提交命令提交

--- src/MainView.js
+++ src/MainView.js
@@ -1,23 +1,12 @@
-
-import { useState } from 'react'
-
-import Header from './Header'
-import Fotter from './Fotter'
-import Feature from './Feature'
-import Newsletter from './Newsletter'
-import Point from './Point'
-import Testimonials from './Testimonials'
-
-
-import Hero from './Hero'
-
-
-
-
+import Header from './Views/Header'
+import Fotter from './Views/Fotter'
+import Feature from './Views/Feature'
+import Newsletter from './Views/Newsletter'
+import Point from './Views/Point'
+import Testimonials from './Views/Testimonials'
+import Hero from './Views/Hero'
 
 export default function MainView() {
-    const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
-
     return (
         <div className="bg-white">
             {/* Header */}
@@ -25,13 +14,10 @@ export default function MainView() {
             <main>
                 {/* Hero section */}
                 <Hero></Hero>
-
                 {/* Feature section */}
-                {/* <Feature></Feature> */}
-
+                <Feature></Feature>
                 {/* Feature section */}
                 <Point></Point>
-
                 {/* Newsletter section */}
                 <Newsletter></Newsletter>
                 {/* Testimonials section */}

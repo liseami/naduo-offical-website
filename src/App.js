17e6c630921773cea7515c2ef5887066cb0ef179
zhaoxiangyu <liseami@qq.com>
2023-04-12 10:19:51 +0800

    zsh快捷提交命令提交

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 
 import './App.css';
-import Hero from './HeroView';
+import MainView from './MainView';
 
 function App() {
   return (
-    <Hero></Hero>
+    <MainView />
   );
 }
 
---
+++ src/MainView.js
@@ -0,0 +1,44 @@
+
+import { useState } from 'react'
+
+import Header from './Header'
+import Fotter from './Fotter'
+import Feature from './Feature'
+import Newsletter from './Newsletter'
+import Point from './Point'
+import Testimonials from './Testimonials'
+
+
+import Hero from './Hero'
+
+
+
+
+
+export default function MainView() {
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+
+    return (
+        <div className="bg-white">
+            {/* Header */}
+            <Header></Header>
+            <main>
+                {/* Hero section */}
+                <Hero></Hero>
+
+                {/* Feature section */}
+                <Feature></Feature>
+
+                {/* Feature section */}
+                <Point></Point>
+
+                {/* Newsletter section */}
+                <Newsletter></Newsletter>
+                {/* Testimonials section */}
+                <Testimonials></Testimonials>
+            </main>
+            {/* Footer */}
+            <Fotter></Fotter>
+        </div>
+    )
+}
